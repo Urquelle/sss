a077f38c6fba207ef71964783ec539396f23b20b
Serge Ratke <serge.ratke@gmail.com>
2021-05-16 14:23:44 +0200

    bat korrigiert

--- bin/shell.bat
+++ bin/shell.bat
@@ -3,6 +3,6 @@
 SET PROJECT_NAME=sss
 SET PROJECT_LINKER_FLAGS=user32.lib gdi32.lib winmm.lib Shlwapi.lib Opengl32.lib windowscodecs.lib Ole32.lib Shell32.lib
 
-SET PATH=C:\Users\serge\Dev\Cpp\bin;%PATH%
+SET PATH=%UserProfile%\Dev\Cpp\bin;%PATH%
 
 call global_shell.bat
