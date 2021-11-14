d497b0f5459f831abe37fce0b0f7838af7a5ed0f
Serge Ratke <43104+Urquelle@users.noreply.github.com>
2021-11-14 12:29:58 +0100

    meta

--- bin/github_build_test.bat
+++ bin/github_build_test.bat
@@ -1,6 +1,7 @@
-set compiler_flags=-Od -MTd -nologo -fp:fast -fp:except- -EHsc -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -wd4201 -wd4100 -wd4101 -wd4189 -wd4505 -wd4127 -wd4702 -wd4530 -wd4800 -FC -Z7 -I../src
-set linker_flags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib Shlwapi.lib Ws2_32.lib
-cl %compiler_flags% ..\tests\tests.cpp -Fetests.exe /link %linker_flags%
+set compiler_flags=-Od -D_CRT_SECURE_NO_WARNINGS -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -wd4624 -wd4530 -wd4201 -wd4100 -wd4101 -wd4189 -wd4505 -wd4127 -wd4702 -wd4310 -FC -Z7 -Isrc -Ithird_party/dyncall-1.2-release/include
+
+set linker_flags= -libpath:third_party/dyncall-1.2-release/lib -incremental:no -nodefaultlib:LIBCMT -opt:ref
+cl %compiler_flags% tests\tests.cpp -Fetests.exe /link %linker_flags%
 
 if %ERRORLEVEL% NEQ 0 (
     echo "fehler bei der kompilierung des projekts"
